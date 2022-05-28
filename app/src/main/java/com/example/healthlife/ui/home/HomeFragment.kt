2c543fe71cbb18c4954be3a16f121ca3e40a0a12
wangxinyu <2672399599@qq.com>
2022-05-28 09:30:15 +0800

    add some  function for homefragment

--- app/src/main/java/com/example/healthlife/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/healthlife/ui/home/HomeFragment.kt
@@ -5,29 +5,52 @@ import android.view.View
 import android.widget.Toast
 import androidx.activity.OnBackPressedCallback
 import com.bumptech.glide.Glide
+import com.example.healthlife.R
 import com.example.healthlife.base.fragment.BaseViewBindingFragment
 import com.example.healthlife.databinding.FragmentHomeBinding
+import com.example.healthlife.uistate.FeatureUiState
 import com.youth.banner.Banner
 import com.youth.banner.adapter.BannerImageAdapter
 import com.youth.banner.holder.BannerImageHolder
 import com.youth.banner.indicator.CircleIndicator
 
 class HomeFragment : BaseViewBindingFragment<FragmentHomeBinding>(FragmentHomeBinding::inflate) {
-    var imageUrls = listOf(
-        "https://img.zcool.cn/community/01b72057a7e0790000018c1bf4fce0.png",
-        "https://img.zcool.cn/community/016a2256fb63006ac7257948f83349.jpg",
-        "https://img.zcool.cn/community/01233056fb62fe32f875a9447400e1.jpg",
-        "https://img.zcool.cn/community/01700557a7f42f0000018c1bd6eb23.jpg"
-    )
-
+   private lateinit var featuresAdapter: FeaturesAdapter
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        setOnBackPress()
+        setListener()
+        featuresAdapter = FeaturesAdapter()
+        viewBinding.rvFeaturesList.adapter = featuresAdapter
+        featuresAdapter.submitList(getFeaturesList())
+    }
+
+    private fun getFeaturesList(): MutableList<FeatureUiState> {
+        return mutableListOf(
+            FeatureUiState(R.drawable.ic_mall, getString(R.string.sport_mall), 0),
+            FeatureUiState(R.drawable.ic_mall, getString(R.string.sport_mall), 1),
+            FeatureUiState(R.drawable.ic_mall, getString(R.string.sport_mall), 2),
+            FeatureUiState(R.drawable.ic_mall, getString(R.string.sport_mall), 3),
+            FeatureUiState(R.drawable.ic_mall, getString(R.string.sport_mall), 4),
+            FeatureUiState(R.drawable.ic_mall, getString(R.string.sport_mall), 5),
+            FeatureUiState(R.drawable.ic_mall, getString(R.string.sport_mall), 6),
+            FeatureUiState(R.drawable.ic_mall, getString(R.string.sport_mall), 7)
+        )
+    }
+
+    private fun setOnBackPress() {
         requireActivity().onBackPressedDispatcher
-            .addCallback(viewLifecycleOwner, object : OnBackPressedCallback(true) {
-                override fun handleOnBackPressed() {
-                    requireActivity().finish()
-                }
-            })
+            .addCallback(
+                viewLifecycleOwner,
+                object : OnBackPressedCallback(true) {
+                    override fun handleOnBackPressed() {
+                        requireActivity().finish()
+                    }
+                },
+            )
+    }
+
+    private fun setListener() {
         val banner: Banner<String, BannerImageAdapter<String>> =
             viewBinding.banner as Banner<String, BannerImageAdapter<String>>
         banner.apply {
@@ -47,6 +70,7 @@ class HomeFragment : BaseViewBindingFragment<FragmentHomeBinding>(FragmentHomeBi
             })
 
             banner.setOnBannerListener { data, position ->
+                logger.d(TAG, "data : $data")
                 Toast.makeText(
                     requireContext(),
                     "点击了当前第 $position 个",
@@ -55,4 +79,15 @@ class HomeFragment : BaseViewBindingFragment<FragmentHomeBinding>(FragmentHomeBi
             }
         }
     }
+
+    companion object {
+        const val TAG = "HomeFragment"
+        val imageUrls = listOf(
+            "https://img.zcool.cn/community/01b72057a7e0790000018c1bf4fce0.png",
+            "https://img.zcool.cn/community/016a2256fb63006ac7257948f83349.jpg",
+            "https://img.zcool.cn/community/01233056fb62fe32f875a9447400e1.jpg",
+            "https://img.zcool.cn/community/01700557a7f42f0000018c1bd6eb23.jpg"
+        )
+    }
+
 }
--- app/src/main/java/com/example/healthlife/uistate/FeatureUiState.kt
+++ app/src/main/java/com/example/healthlife/uistate/FeatureUiState.kt
@@ -1,9 +1,8 @@
 package com.example.healthlife.uistate
 
-import androidx.annotation.IdRes
-
-class FeatureUiState(
-    @field:IdRes @param:IdRes val imageResId: Int,
+data class FeatureUiState(
+    val imageResId: Int,
     val text: String,
     val position: Int
+
 )
